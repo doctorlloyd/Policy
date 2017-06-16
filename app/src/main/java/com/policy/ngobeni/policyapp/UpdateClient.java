588e65b86bca1020957ef75a997737f34da3a677
Doc <doctorlloyd9@gmail.com>
2017-06-15 17:45:52 -0700

    update

--- app/src/main/java/com/policy/ngobeni/policyapp/UpdateClient.java
+++ app/src/main/java/com/policy/ngobeni/policyapp/UpdateClient.java
@@ -6,11 +6,18 @@ import android.os.Bundle;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.policy.ngobeni.policyapp.pojos.Client;
 
 public class UpdateClient extends AppCompatActivity {
     private String _key = "";
+    Client client = new Client();
     private FirebaseUser _fbuser;
     private DatabaseReference _databaseReference;
     private StorageReference _storageReference;
@@ -22,9 +29,26 @@ public class UpdateClient extends AppCompatActivity {
         setContentView(R.layout.update_client);
 
         _key = getIntent().getStringExtra("key");
+
+        //INITIALIZING FIREBASE CONTENT
+        _storageReference = FirebaseStorage.getInstance().getReference();
+        _databaseReference = FirebaseDatabase.getInstance().getReference().child("Clients").child(_key);
+        FirebaseAuth _user = FirebaseAuth.getInstance();
+        _fbuser = _user.getCurrentUser();
+
+        _databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                client = (Client) dataSnapshot.getValue();
+                //TODO populate the views with data
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
-
-
     @Override
     public void onBackPressed() {
         startActivity(new Intent(getBaseContext(),Home.class));
